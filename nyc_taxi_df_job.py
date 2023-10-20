0a51268153d123cb088dcbc2601ae077f436dcd9
Adhi Nugroho <adhi.nugroho@email.com>
2023-10-20 18:33:16 +0100

    Column renaming

--- nyc_taxi_df_job.py
+++ nyc_taxi_df_job.py
@@ -1,10 +1,25 @@
 import apache_beam as beam
 
 
+def rename_datetime_cols(element):
+    element['pickup_datetime'] = element.pop('tpep_pickup_datetime')
+    element['dropoff_datetime'] = element.pop('tpep_dropoff_datetime')
+    return element
+
+
+def formatting_col_name(element):
+    element['vendor_id'] = element.pop('VendorID')
+    element['rate_code_id'] = element.pop('RatecodeID')
+    element['pu_location_id'] = element.pop('PULocationID')
+    element['do_location_id'] = element.pop('DOLocationID')
+    element['airport_fee'] = element.pop('Airport_fee')
+    return element
+
+
 def generate_nyc_taxi_data():
     with beam.Pipeline() as p:
-        p | beam.io.ReadFromParquet(
-            './taxi_sample.parquet-00000-of-00001') | beam.Map(print)
+        raw_input_pcoll = p | beam.io.ReadFromParquet(
+            './taxi_sample.parquet-00000-of-00001') | beam.Map(rename_datetime_cols) | beam.Map(formatting_col_name) | beam.Map(print)
 
 
 if __name__ == "__main__":

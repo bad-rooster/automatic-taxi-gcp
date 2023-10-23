db2e10a686d4ca8c8c60cfcaa26f3f5544f84e63
Adhi Nugroho <adhi.nugroho@email.com>
2023-10-23 13:08:43 +0100

    Changing dataflow template and deploy sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# trigger wordcount dataflow
+python -m wordcount \
+    --region eurpoe-west2 \
+    --input gs://dataflow-samples/shakespeare/kinglear.txt \
+    --output gs://wordcount-kinglear/results/outputs \
+    --runner DataflowRunner \
+    --project polar-storm-402611 \
+    --temp_location gs://wordcount-kinglear/tmp/
+
+# trigger nyc taxi dataflow
+python -m nyc_taxi_df_job \
+    --region europe-west2 \
+    --input gs://dataflow-nyc-taxi-parquet-an/inputs/taxi_sample.parquet \
+    --output gs://dataflow-nyc-taxi-parquet-an/results/taxi_rides \
+    --runner DataflowRunner \
+    --project polar-storm-402611 \
+    --temp_location gs://dataflow-nyc-taxi-parquet-an/tmp/
--- nyc_taxi_df_job.py
+++ nyc_taxi_df_job.py
@@ -1,4 +1,29 @@
 import apache_beam as beam
+import pyarrow
+import argparse
+from datetime import datetime
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
+
+
+taxi_schema = pyarrow.schema([('vendor_id', pyarrow.int32()),
+                             ('pickup_datetime', pyarrow.timestamp('us')),
+                             ('dropoff_datetime', pyarrow.timestamp('us')),
+                             ('passenger_count', pyarrow.int32()),
+                             ('trip_distance', pyarrow.float32()),
+                             ('rate_code_id', pyarrow.int32()),
+                             ('store_and_fwd_flag', pyarrow.large_string()),
+                             ('pu_location_id', pyarrow.int64()),
+                             ('do_location_id', pyarrow.int64()),
+                             ('payment_type', pyarrow.int32()),
+                             ('fare_amount', pyarrow.float32()),
+                             ('extra', pyarrow.float32()),
+                             ('mta_tax', pyarrow.float32()),
+                             ('tip_amount', pyarrow.float32()),
+                             ('tolls_amount', pyarrow.float32()),
+                             ('improvement_surcharge', pyarrow.float32()),
+                             ('total_amount', pyarrow.float32()),
+                             ('congestion_surcharge', pyarrow.float32()),
+                             ('airport_fee', pyarrow.float32())])
 
 
 def rename_datetime_cols(element):
@@ -16,10 +41,30 @@ def formatting_col_name(element):
     return element
 
 
-def generate_nyc_taxi_data():
+def generate_nyc_taxi_data(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input',
+                        required=True,
+                        dest='input'
+                        )
+    parser.add_argument('--output',
+                        required=True,
+                        dest='output'
+                        )
+
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+
+    datetime_stamp = datetime.now().strftime('%Y-%m-%d-%H%M%S')
+    output_name = f"{known_args.output}-{datetime_stamp}"
     with beam.Pipeline() as p:
-        raw_input_pcoll = p | beam.io.ReadFromParquet(
-            './taxi_sample.parquet-00000-of-00001') | beam.Map(rename_datetime_cols) | beam.Map(formatting_col_name) | beam.Map(print)
+        (p
+         | beam.io.ReadFromParquet(known_args.input)
+         | beam.Map(rename_datetime_cols)
+         | beam.Map(formatting_col_name)
+         | beam.io.WriteToParquet(output_name, schema=taxi_schema, file_name_suffix='.parquet')
+         )
 
 
 if __name__ == "__main__":
